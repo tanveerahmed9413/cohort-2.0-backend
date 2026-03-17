1f6da5466b95a9047908b7a1e4705278fcef6fd7
tanveerahmed9413 <tanveerahmed4872@gmail.com>
2026-03-17 21:14:24 +0530

    posts create

--- insta-clone/backend/src/controllers/post.controller.js
+++ insta-clone/backend/src/controllers/post.controller.js
@@ -1,5 +1,5 @@
 const postModel = require('../models/post.model')
-
+const jwt = require('jsonwebtoken')
 const ImageKit = require('@imagekit/nodejs')
 const {toFile} = require('@imagekit/nodejs')
 
@@ -9,13 +9,48 @@ const imageKit = ImageKit({
 
 
 async function postCreate(req,res) {
-    console.log(req.body, req.file)
+
+    let token =  req.cookies.token
+
+    if(!token){
+    return    res.status(401).json({
+            message: 'token not provided, unouthrized access'
+        })
+    }
+
+
+    let decoded;
+
+    try{
+         decoded = jwt.verify(token,process.env.JWT_SECRET)
+    }
+    catch(err){
+     return   res.status(401).json({
+            message: 'user not authorized'
+        })
+        
+    }
 
     let file = await imageKit.files.upload({
         file: await toFile(req.file.buffer,'file'),
-        fileName: 'post-image'
+        fileName: 'post-image',
+        folder: 'cohort-2-insta-clone-posts'
     })
-    res.send(file)
+  
+
+
+let post = await postModel.create({
+    caption: req.body.caption,
+    imageUrl: file.url,
+    user: decoded.id
+  
+})
+
+res.status(201).json({
+    message: 'post create successfully',
+    post
+})
+
 }
 
 
