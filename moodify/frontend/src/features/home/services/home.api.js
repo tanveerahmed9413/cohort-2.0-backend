ce816fc4d5dfe90382140570a2481ddbcb772bbe
tanveerahmed9413 <tanveerahmed4872@gmail.com>
2026-08-19 08:48:35 +0530

    change upload song file

--- moodify/frontend/src/features/home/services/home.api.js
+++ moodify/frontend/src/features/home/services/home.api.js
@@ -12,13 +12,20 @@ export async function getAllSongs() {
 
 export const getSongsByMood = async (mood) => {
   const response = await api.get(`/api/songs/mood/${mood}`);
-  console.log(response.data.songs)
+  console.log(response.data.songs);
   return response.data.songs;
 };
 
 export async function songUpload(songFile) {
-  const formData = new FormData();
-  formData.append("song", songFile);
-  const response = await api.post("/api/songs/upload", formData);
-  return response.data;
+  try {
+    const formData = new FormData();
+    formData.append("song", songFile);
+    const response = await api.post("/api/songs/upload", formData);
+
+    return response.data;
+  } catch (error) {
+    console.error("UPLOAD ERROR:", error.message);
+
+    throw error;
+  }
 }
